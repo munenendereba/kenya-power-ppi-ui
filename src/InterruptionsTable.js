80899bd1c3b712d523a3db4ed7fb507e47ecf223
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-10-17 09:38:02 +0300

    search term working

--- src/InterruptionsTable.js
+++ src/InterruptionsTable.js
@@ -3,12 +3,15 @@ import paginationFactory from "react-bootstrap-table2-paginator";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const API_URL = "http://localhost:3000";
+
 function InterruptionsTable() {
   const [datas, setData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/interruption")
+      .get(API_URL + "/interruption")
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
   }, []);
@@ -30,7 +33,7 @@ function InterruptionsTable() {
     hideSizePerPage: true,
     hidePageListOnlyOnePage: true,
     alwaysShowAllBtns: false,
-    withFirstAndLast: false,
+    withFirstAndLast: true,
   };
 
   if (!datas)
@@ -39,9 +42,40 @@ function InterruptionsTable() {
         <h1>Loading...</h1>
       </div>
     );
+  function SearchTable(event) {
+    //on search, hide the rows that don't match the search by locations column
+    setSearchTerm(event.target.value);
+
+    let filter, table, tr, td, i, txtValue;
+
+    if (!searchTerm || searchTerm.length < 1) return;
+
+    filter = searchTerm.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[4];
+      if (td) {
+        txtValue = (td.textContent || td.innerText).toUpperCase();
+
+        if (txtValue.indexOf(filter) > -1) tr[i].style.display = "";
+        else tr[i].style.display = "none";
+      }
+    }
+  }
 
   return (
-    <div>
+    <div style={{ backgroundColor: "aqua", textAlign: "center" }} id="table">
+      <h1>Interruptions Table</h1>
+      <input
+        id="search-box"
+        className="form-control ml-4"
+        style={{ width: "90%", margin: "auto" }}
+        type="text"
+        placeholder="Search"
+        onChange={SearchTable}
+      />
+      <br />
       <BootstrapTable
         keyField="id"
         data={datas}
--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import InterruptionsTable from "./InterruptionsTable";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const tableContents = ReactDOM.createRoot(
+  document.getElementById("tableContent")
+);
+
+tableContents.render(
   <React.StrictMode>
     <InterruptionsTable />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
